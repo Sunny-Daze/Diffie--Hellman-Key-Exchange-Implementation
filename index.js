1d9ba2eac0f2b329e35f33bdb1fe4badab4bca79
Sunny-Daze7 <sunnyadav0727@gmail.com>
2022-03-21 01:47:36 +0530

    Final Commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const element = document.getElementById("calculate");
 const displayAns = document.getElementById("ans");
 
 element.addEventListener("click", () => {
-  let getAlice = document.getElementById("aliceKey").value;
+  const getAlice = document.getElementById("aliceKey").value;
+  const ans = document.getElementById("tempAns");
   let p = 23,
     g = 9;
   let xa = getAlice,
@@ -12,9 +13,9 @@ element.addEventListener("click", () => {
   let ybx = Math.pow(yb, xa) % p;
   let yby = Math.pow(ya, xb) % p;
 
-  console.log(getAlice);
-  displayAns.innerHTML = `
-     <h5>Given :</h5>
+  if (getAlice) {
+    displayAns.innerHTML = `
+              <h5>Given :</h5>
               <p>p = ${p}</p>
               <p>g = ${g}</p>
               <p>Private key of Alice (X<sub>A</sub>) = ${getAlice}</p>
@@ -47,6 +48,19 @@ element.addEventListener("click", () => {
               <p>${ya}<sup>${xb}</sup> mod ${p}</p>
               <p>= ${yby}</p>
               <p>Finally, both the parties obtain the same value of secret key.</p>
-              <h4>Ther value of common secret key = <strong>${yby}</strong></h4>
+              <h4>The value of common secret key = <strong>${yby}</strong></h4>
     `;
+    ans.innerHTML = `<h5>Therefore, the shared key is <strong>${yby}</strong></h5>`;
+  } else {
+    displayAns.innerHTML = `
+     <div class="displayWarning">
+        <div class="alert alert-danger text-center my-5" role="alert">
+                <h5>
+                  Please enter the private key of Alice to calculate the value of
+                  secret key.
+                </h5>
+              </div>
+              </div>
+       `;
+  }
 });
